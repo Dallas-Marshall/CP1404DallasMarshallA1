44216b16caab25bd91940da376e314f464f84514
Dallas Marshall <dallas.marshall@my.jcu.edu.au>
2019-08-29 20:06:07 +1000

    Add start to list code

--- assignment1.py
+++ assignment1.py
@@ -7,27 +7,36 @@ GitHub URL: https://github.com/cp1404-students/2019-2-a1-Dallas-Marshall
 
 # import file
 in_file = open('movies.csv', 'r')
-for i, movie in enumerate(in_file):
-    line = in_file.readlines()
-MOVIES_LOADED = len(line)
+movies_data = in_file.readlines()
+movies = [movie.strip().split(',') for movie in movies_data]
+movies_loaded = len(movies)
 VALID_MENU_OPTIONS = "QAWL"
 MENU = """Menu:\nL - List movies\nA - Add new movie\nW - Watch a movie\nQ - Quit""".format(
-    MOVIES_LOADED)
+    movies_loaded)
 
 
 def main():
     """program is a list of movies that allows a user to track movies that they have watched and wish to watch."""
     print("Movies To Watch 1.0 - by Dallas Marshall")
-    print("{} movies loaded".format(MOVIES_LOADED))
+    print("{} movies loaded".format(movies_loaded))
     menu_selection = get_user_input()
     while menu_selection != 'Q':
         if menu_selection == 'L':
-            print("Need to write code for displaying movies")
+            # iterate through movies and if not watched assign asterisk else add space to keep lined up
+            for index in range(len(movies)):
+                if movies[index][3] == 'u':
+                    movies[index][0] = '* ' + movies[index][0]
+                else:
+                    movies[index][0] = '  ' + movies[index][0]
+            # print(movies)
+            for i in range(movies_loaded):
+                print("{}. {:40} - {:5} ({})".format(i, movies[i][0], movies[i][1], movies[i][2]))
         elif menu_selection == "A":
             print("Need to write code for adding movies")
         else:
             print("Need to write code for watching movies")
         menu_selection = get_user_input()
+
 
 def get_user_input():
     print(MENU)
